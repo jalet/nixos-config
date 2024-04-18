237f908538a6d238ddc5bff076510dd1837cb698
Joakim Jarsäter <j@jarsater.com>
2024-04-18 12:29:33 +0200

    remap capslock to escape

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -74,11 +74,11 @@ in {
       };
 
       dock = {
-        autohide = false;
+        autohide = true;
         show-recents = false;
-        launchanim = true;
+        launchanim = false;
         orientation = "bottom";
-        tilesize = 48;
+        tilesize = 24;
       };
 
       loginwindow = {
@@ -104,7 +104,7 @@ in {
 
     keyboard = {
       enableKeyMapping = true;
-      remapCapsLockToControl = true;
+      remapCapsLockToEscape = true;
     };
   };
 
