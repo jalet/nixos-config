f61391bd9f5c2f9e0224548224aa4e0d012faa2c
Joakim Jarsäter <j@jarsater.com>
2024-05-14 15:16:04 +0200

    fix: listen on input spurce change events
    This will listen for events instead of polling for changes
    which should be more performant.

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -305,15 +305,21 @@ in {
     # volume is registered. More info about the event system can be found here:
     # https://felixkratz.github.io/SketchyBar/config/events
 
-    sketchybar --add item clock right \
-               --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh" \
-               --add item keyboard right \
-               --set keyboard update_freq=1 icon=  script="$PLUGIN_DIR/kblayout.sh" \
-               --add item volume right \
-               --set volume script="$PLUGIN_DIR/volume.sh" \
-               --subscribe volume volume_change \
-               --add item battery right \
-               --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh" \
+    sketchybar --add event keyboard_input_source_change AppleSelectedInputSourcesChangedNotification
+
+    sketchybar --add item clock right                                                     \
+               --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh"           \
+                                                                                          \
+               --add item keyboard right                                                  \
+               --set keyboard icon=  script="$PLUGIN_DIR/kblayout.sh"                    \
+               --subscribe keyboard keyboard_input_source_change                          \
+                                                                                          \
+               --add item volume right                                                    \
+               --set volume script="$PLUGIN_DIR/volume.sh"                                \
+               --subscribe volume volume_change                                           \
+                                                                                          \
+               --add item battery right                                                   \
+               --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh"              \
                --subscribe battery system_woke power_source_change
 
     ##### Force all scripts to run the first time (never do this in a script) #####
